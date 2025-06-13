a6c81cd68b21b60f7b92f4e1d3031ffd3fee36bc
Gerald Blanco <gblanco0504@gmail.com>
2025-06-13 16:22:42 -0600

    Initial Commit

---
+++ LearnFundamentals/LearnFundamentals/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace LearnFundamentals.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string NameProduct { get; set; }
+        public string DescProduct { get; set; }
+        public double price { get; set; }
+    }
+}
